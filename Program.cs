72c97a1d26d66dc0b1a9d6a2aa1662eaf27a562c
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-10-09 13:23:55 +0200

    "Add AddNumbers"

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example
+{
+    public class Calculator
+    {
+        // First method
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            int result1 = calc.AddNumbers(5, 3);
+
+            Console.WriteLine($"AddNumbers result: {result1}");
+        }
+    }
+}
